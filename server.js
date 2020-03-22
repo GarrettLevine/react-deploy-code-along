2c7d3200ee6b64b43be0e611c5837275ae04452a
GarrettLevine <garrlevine@gmail.com>
2020-03-22 12:20:38 -0400

    makes changes for deploy

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const userRouter = require('./api/routes/users/userRoutes');
 const notesRouter = require('./api/routes/notes/notesRoutes');
 
+const PORT = process.env.PORT || 8080;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/note-app';
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,16 +20,15 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-
 mongoose
-  .connect('mongodb://localhost:27017/note-app',
+  .connect(MONGODB_URI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
   )
   .then(() => {
-    app.listen('8080', () => {
+    app.listen(PORT, () => {
       console.log('server is running on port 8080');
     });
   })
